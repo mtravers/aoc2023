c9903c71c8df41aad9909c201cf38351bbe4fb2b
Mike Travers <mt@hyperphor.com>
2023-12-09 08:58:50 -0800

    day 7 part 2

--- src/aoc2023/day7.clj
+++ src/aoc2023/day7.clj
@@ -52,3 +52,61 @@
                          (* score (+ i 1)))
                        (rank-hands hands))))
   
+
+;;; Part 2
+
+(def cards2 "AKQT98765432J")
+
+(defn card-rank-2
+  [c]
+  (- (s/index-of cards2 c)))
+
+;;; Multitool! But needs a better implementation
+(defn bag=
+  [seq1 seq2]
+  (and (= (count seq1) (count seq2))
+       (= (sort seq1) (sort seq2))))
+
+;;; Full house cases AAABB JAABB
+(defn full-house
+  [freqs]
+  (or (bag= (vals freqs) [3 2])
+      (and (= 1 (get freqs \J))
+           (bag= (vals freqs) [1 2 2]))))
+  
+;;; You can't have a two-pair hand with a J so this is simple
+(defn two-pair
+  [freqs]
+  (bag= (vals freqs) [2 2 1]))
+
+(defn rank-hand-2
+  [h]
+  (let [freqs (frequencies h)
+        non-j-freqs (dissoc freqs \J)
+        jokers (get freqs \J 0)
+        with (fn [n] (u/some-thing (fn [[c f]] (= f n)) non-j-freqs))
+        with-jokers (fn [n] (with (- n jokers)))
+        type
+        (cond (or (with-jokers 5) (= h "JJJJJ")) 0
+              (with-jokers 4) -1
+              (full-house freqs) -2 
+              (with-jokers 3) -3
+              (two-pair freqs) -4
+              (with-jokers 2) -5
+              :else -6)]
+    (apply vector type (map card-rank-2 h) )))
+
+;;; Copying rather than parameterizing because lazy
+(defn rank-hands-2
+  [hands]
+  (let [hands (map #(conj % (rank-hand-2 (first %))) hands)]
+    (sort-by #(nth % 2) hands)))
+
+(defn score-hands-2
+  [hands]
+  (reduce +
+          (map-indexed (fn [i [_ score _]]
+                         (* score (+ i 1)))
+                       (rank-hands-2 hands))))
+
+
